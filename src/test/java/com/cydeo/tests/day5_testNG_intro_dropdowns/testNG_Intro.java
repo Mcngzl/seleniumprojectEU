39a9e3ff0bbc0e8dcdf4ccd3bc0f0f6c31080a26
mcengiz <cengizulu@gmail.com>
2022-05-16 00:33:51 +0200

    day5 TestNG

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/testNG_Intro.java
@@ -0,0 +1,16 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import org.testng.annotations.Test;
+
+public class testNG_Intro {
+
+
+    @Test
+    public void test1(){
+        System.out.println("Test1 is running...");
+    }
+
+
+
+
+}
