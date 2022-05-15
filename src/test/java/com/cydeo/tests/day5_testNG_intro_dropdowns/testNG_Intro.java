35a55b3cbcf81b2e95d1412e7367a8e0c2eac1c1
mcengiz <cengizulu@gmail.com>
2022-05-16 01:19:51 +0200

    day5 TestNG

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/testNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/testNG_Intro.java
@@ -1,16 +1,53 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class testNG_Intro {
 
+    @BeforeClass
+    public void setupMethod(){
+        System.out.println("-----> BeforeClass is running!");
+    }
 
-    @Test
-    public void test1(){
-        System.out.println("Test1 is running...");
+    @AfterClass
+    public void tearDownClass(){
+        System.out.println("--->AfterClass is running!");
     }
 
 
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println("---> Before method is running!");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("--->AfterMethod is running!");
+    }
+
+
+    @Test(priority = 1)
+    public void test1(){
+        System.out.println("Test1 is running...");
+
+        //Assert Equals :compare two things
+        String actual ="apple";
+        String expected ="apple";
+
+        Assert.assertEquals(actual,expected);
+
+    }
+
+
+    @Test(priority = 2)
+    public void Test2(){
+        System.out.println("Test2 is running...");
+        String actual ="apple";
+        String expected ="apple";
+
+        Assert.assertTrue(actual.equals(expected));
+    }
 
 
 }
